53596803b29a6f5b0c494e8ac578141ff1526a20
huangyili <1395614093@qq.com>
2025-01-19 20:02:19 +0800

    first commit

---
+++ src/java/GitTest2.java
@@ -0,0 +1,10 @@
+package java;
+
+public class GitTest2 {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+    }
+}
